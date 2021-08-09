fbacfc72242856d55c7e59c5d7f53fcf15538048
Debox-Mehank <mehankjain1670@gmail.com>
2021-08-09 10:05:26 +0530

    mcq added timer

--- lib/objectiveTest.dart
+++ lib/objectiveTest.dart
@@ -12,7 +12,25 @@ class ObjectiveTest extends StatefulWidget {
   _ObjectiveTestState createState() => _ObjectiveTestState();
 }
 
-class _ObjectiveTestState extends State<ObjectiveTest> {
+class _ObjectiveTestState extends State<ObjectiveTest>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: Duration(seconds: 60), vsync: this);
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward().whenComplete(nextQuestion);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   List<Question> _questions = sample_data
       .map(
         (question) => Question(
@@ -33,14 +51,14 @@ class _ObjectiveTestState extends State<ObjectiveTest> {
   bool showScore = false;
 
   void checkAns(Question question, int selectedIndex) {
-    print(question.question);
-    print(selectedIndex);
     setState(() {
       isAnswered = true;
       correctAns = question.answer;
       selectedAns = selectedIndex;
 
       if (correctAns == selectedAns) numOfCorrectAns++;
+
+      _controller.stop();
 
       Future.delayed(Duration(seconds: 3), () {
         nextQuestion();
@@ -55,6 +73,10 @@ class _ObjectiveTestState extends State<ObjectiveTest> {
       });
       _pageController.nextPage(
           duration: Duration(milliseconds: 250), curve: Curves.ease);
+
+      _controller.reset();
+
+      _controller.forward().whenComplete(nextQuestion);
     } else {
       setState(() {
         showScore = true;
@@ -70,200 +92,164 @@ class _ObjectiveTestState extends State<ObjectiveTest> {
 
   @override
   Widget build(BuildContext context) {
-    if (showScore) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Objective Test Series'),
-          ),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Your Score",
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "${correctAns * 10}/${_questions.length * 10}",
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 15,
-                        color: Colors.black,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ));
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Objective Test Series'),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
-            child: Column(
-              children: [
-                Expanded(
-                  child: PageView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    controller: _pageController,
-                    onPageChanged: updateTheQnNum,
-                    itemCount: _questions.length,
-                    itemBuilder: (context, index1) => Expanded(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 12, horizontal: 12),
-                          padding: EdgeInsets.all(12),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Objective Test Series'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFFF7801), width: 1),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Stack(
+                    children: [
+                      // LayoutBuilder provide us the available space for the conatiner
+                      // constraints.maxWidth needed for our animation
+                      LayoutBuilder(
+                        builder: (context, constraints) => Container(
+                          // from 0 to 1 it takes 60s
+                          height: 34,
+                          width: constraints.maxWidth * _animation.value,
                           decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            gradient: LinearGradient(
+                              colors: [Color(0xFFFF7801), Color(0xFFFF7801)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.symmetric(horizontal: 12 / 2),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                _questions[index1].question,
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              ...List.generate(
-                                  _questions[index1].options.length, (index) {
-                                Color getTheRightColor() {
-                                  if (isAnswered) {
-                                    if (index == correctAns) {
-                                      return Color(0xFF6AC259);
-                                    } else if (index == selectedAns &&
-                                        selectedAns != correctAns) {
-                                      return Color(0xFFE92E30);
-                                    }
-                                  }
-                                  return Color(0xFFC1C1C1);
-                                }
-
-                                IconData getTheRightIcon() {
-                                  return getTheRightColor() == Color(0xFFE92E30)
-                                      ? Icons.close
-                                      : Icons.done;
-                                }
-
-                                return InkWell(
-                                  onTap: () =>
-                                      checkAns(_questions[index1], index),
-                                  child: Container(
-                                    margin: EdgeInsets.only(top: 25),
-                                    padding: EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                      border:
-                                          Border.all(color: getTheRightColor()),
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "${index + 1}. ${_questions[index1].options[index]}",
-                                          style: TextStyle(
-                                              fontFamily: 'Poppins',
-                                              fontSize: 14,
-                                              color: Colors.black),
-                                        ),
-                                        Container(
-                                          height: 26,
-                                          width: 26,
-                                          decoration: BoxDecoration(
-                                            color: getTheRightColor() ==
-                                                    Color(0xFFC1C1C1)
-                                                ? Colors.transparent
-                                                : getTheRightColor(),
-                                            borderRadius:
-                                                BorderRadius.circular(50),
-                                            border: Border.all(
-                                                color: getTheRightColor()),
-                                          ),
-                                          child: getTheRightColor() ==
-                                                  Color(0xFFC1C1C1)
-                                              ? null
-                                              : Icon(getTheRightIcon(),
-                                                  size: 16),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              })
+                              Text("${(_animation.value * 60).round()} sec",
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
                             ],
                           ),
-                        )),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //   children: [
-                //     NeumorphicButton(
-                //       margin: EdgeInsets.only(top: 12),
-                //       onPressed: () {
-                //         _pageController.previousPage(
-                //             duration: Duration(milliseconds: 250),
-                //             curve: Curves.ease);
-                //       },
-                //       style: NeumorphicStyle(
-                //         shape: NeumorphicShape.flat,
-                //         depth: 3,
-                //         color: Color(0xFFFF7801),
-                //         boxShape: NeumorphicBoxShape.roundRect(
-                //             BorderRadius.circular(8)),
-                //       ),
-                //       padding: const EdgeInsets.all(12.0),
-                //       child: Icon(
-                //         Icons.chevron_left,
-                //         color: Colors.white,
-                //         size: 28.0,
-                //         semanticLabel: 'Prev Question',
-                //       ),
-                //     ),
-                //     NeumorphicButton(
-                //         margin: EdgeInsets.only(top: 12),
-                //         onPressed: () {
-                //           _pageController.nextPage(
-                //               duration: Duration(milliseconds: 250),
-                //               curve: Curves.ease);
-                //         },
-                //         style: NeumorphicStyle(
-                //           shape: NeumorphicShape.flat,
-                //           depth: 3,
-                //           color: Color(0xFFFF7801),
-                //           boxShape: NeumorphicBoxShape.roundRect(
-                //               BorderRadius.circular(8)),
-                //         ),
-                //         padding: const EdgeInsets.all(12.0),
-                //         child: Icon(
-                //           Icons.chevron_right,
-                //           color: Colors.white,
-                //           size: 28.0,
-                //           semanticLabel: 'Next Question',
-                //         )),
-                //   ],
-                // )
-              ],
-            ),
+              ),
+              Expanded(
+                child: PageView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  controller: _pageController,
+                  onPageChanged: updateTheQnNum,
+                  itemCount: _questions.length,
+                  itemBuilder: (context, index1) => Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              "Q${index1 + 1}. ${_questions[index1].question}",
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            ...List.generate(_questions[index1].options.length,
+                                (index) {
+                              Color getTheRightColor() {
+                                if (isAnswered) {
+                                  if (index == correctAns) {
+                                    return Color(0xFF6AC259);
+                                  } else if (index == selectedAns &&
+                                      selectedAns != correctAns) {
+                                    return Color(0xFFE92E30);
+                                  }
+                                }
+                                return Color(0xFFC1C1C1);
+                              }
+
+                              IconData getTheRightIcon() {
+                                return getTheRightColor() == Color(0xFFE92E30)
+                                    ? Icons.close
+                                    : Icons.done;
+                              }
+
+                              return InkWell(
+                                onTap: () =>
+                                    checkAns(_questions[index1], index),
+                                child: Container(
+                                  margin: EdgeInsets.only(top: 25),
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border:
+                                        Border.all(color: getTheRightColor()),
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "${index + 1}. ${_questions[index1].options[index]}",
+                                        style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 14,
+                                            color: Colors.black),
+                                      ),
+                                      Container(
+                                        height: 26,
+                                        width: 26,
+                                        decoration: BoxDecoration(
+                                          color: getTheRightColor() ==
+                                                  Color(0xFFC1C1C1)
+                                              ? Colors.transparent
+                                              : getTheRightColor(),
+                                          borderRadius:
+                                              BorderRadius.circular(50),
+                                          border: Border.all(
+                                              color: getTheRightColor()),
+                                        ),
+                                        child: getTheRightColor() ==
+                                                Color(0xFFC1C1C1)
+                                            ? null
+                                            : Icon(getTheRightIcon(), size: 16),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            })
+                          ],
+                        ),
+                      )),
+                ),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
   }
 }
