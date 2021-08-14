8a6e6beb2ac81615cd88e70b9c6982117b624ed4
Debox-Mehank <mehankjain1670@gmail.com>
2021-08-14 10:28:28 +0530

    provider cart logic

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter_virash/newUserRegistration.dart';
 import 'package:flutter_virash/objectiveTest.dart';
 import 'package:flutter_virash/pdfViewer.dart';
 import 'package:flutter_virash/productList.dart';
+import 'package:flutter_virash/providers/cart_provider.dart';
 import 'package:flutter_virash/shopCourse.dart';
 import 'package:flutter_virash/showCart.dart';
 import 'package:flutter_virash/studyMaterial.dart';
@@ -17,6 +18,7 @@ import 'package:flutter_virash/subjectList.dart';
 import 'package:flutter_virash/testSeries.dart';
 import 'package:flutter_virash/whatsappForm.dart';
 import 'package:flutter_virash/verifyOTP.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'homePage.dart';
 import 'loginPage.dart';
@@ -27,8 +29,9 @@ import 'strategyExamList.dart';
 main() async {
   await getSharedPref();
 
-  runApp(
-    MaterialApp(
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => CartProvider())],
+    child: MaterialApp(
       theme: ThemeData(
           fontFamily: 'Poppins',
           scaffoldBackgroundColor: Color(0xFFFFFFFF),
@@ -62,7 +65,7 @@ main() async {
         ExamStrategy.route: (context) => ExamStrategy()
       },
     ),
-  );
+  ));
 }
 
 Future<void> getSharedPref() async {
--- lib/strategyExamList.dart
+++ lib/strategyExamList.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_virash/examStrategy.dart';
-import 'package:flutter_virash/subjectList.dart';
+// import 'package:flutter_virash/subjectList.dart';
 import 'package:http/http.dart';
 import 'dart:async';
 import 'dart:convert';
