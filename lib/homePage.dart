51a8c19b2b086c23172029bdd4b2b6b04233c61a
ParthParekh-dev <parthparekh.dev@gmail.com>
2021-08-27 11:47:35 +0530

    Homescreen UI changes

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -7,6 +7,7 @@ import 'package:flutter_virash/strategyExamList.dart';
 // import 'package:flutter_virash/studyMaterial.dart';
 import 'package:flutter_virash/testSeries.dart';
 import 'package:flutter_virash/whatsappForm.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -41,19 +42,29 @@ class _HomePageState extends State<HomePage> {
                   Expanded(
                     flex: 1,
                     child: Padding(
-                      padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 15),
+                      padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 25),
                       child: GestureDetector(
                         onTap: () {
                           Navigator.pushNamed(context, LiveSession.route);
                         },
                         child: MainCard(
                           cardChild: Center(
-                            child: Text(
-                              'Live Sessions',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 16,
-                                  color: Colors.white),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.ggCircle,
+                                  size: 50,
+                                  color: Color(0xFFFF7801),
+                                ),
+                                Text(
+                                  'Live Sessions',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
                         ),
@@ -61,26 +72,109 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 15),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, ShopCourse.route);
-                          },
-                          child: MainCard(
-                            cardChild: Center(
-                              child: Text(
-                                'All Courses',
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 16,
-                                    color: Colors.white),
-                              ),
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 25),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, ShopCourse.route);
+                        },
+                        child: MainCard(
+                          cardChild: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.graduationCap,
+                                  size: 50,
+                                  color: Color(0xFFFF7801),
+                                ),
+                                Text(
+                                  'All Courses',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      ))
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 25),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, ExamList.route);
+                        },
+                        child: MainCard(
+                          cardChild: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.leanpub,
+                                  size: 50,
+                                  color: Color(0xFFFF7801),
+                                ),
+                                Text(
+                                  'Study Material',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 25),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, TestSeries.route);
+                        },
+                        child: MainCard(
+                          cardChild: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.joomla,
+                                  size: 50,
+                                  color: Color(0xFFFF7801),
+                                ),
+                                Text(
+                                  'Test Series',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -92,96 +186,66 @@ class _HomePageState extends State<HomePage> {
                   Expanded(
                       flex: 1,
                       child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 15),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, ExamList.route);
-                          },
-                          child: MainCard(
-                            cardChild: Center(
-                              child: Text(
-                                'Study Material',
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 16,
-                                    color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )),
-                  Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 15),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, TestSeries.route);
-                          },
-                          child: MainCard(
-                            cardChild: Center(
-                              child: Text(
-                                'Test Series',
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 16,
-                                    color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ))
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 15),
+                        padding: const EdgeInsets.fromLTRB(15, 0, 7.5, 25),
                         child: GestureDetector(
                           onTap: () {
                             Navigator.pushNamed(context, WhatsappForm.route);
                           },
                           child: MainCard(
                             cardChild: Center(
-                              child: Text(
-                                'Whatsapp Groups',
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 16,
-                                    color: Colors.white),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  FaIcon(
+                                    FontAwesomeIcons.whatsapp,
+                                    size: 50,
+                                    color: Color(0xFFFF7801),
+                                  ),
+                                  Text(
+                                    'Whatsapp Groups',
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 16,
+                                        color: Colors.black),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
                         ),
                       )),
                   Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 15),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(
-                                context, StrategyExamList.route);
-                          },
-                          child: MainCard(
-                            cardChild: Center(
-                              child: Text(
-                                'Exam Strategy',
-                                style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 16,
-                                    color: Colors.white),
-                              ),
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(7.5, 0, 15, 25),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, StrategyExamList.route);
+                        },
+                        child: MainCard(
+                          cardChild: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.empire,
+                                  size: 50,
+                                  color: Color(0xFFFF7801),
+                                ),
+                                Text(
+                                  'Exam Strategy',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      ))
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -200,7 +264,7 @@ class MainCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Color(0xFFFF7801),
+      color: Color(0xFFFFFFFF),
       elevation: 20,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
